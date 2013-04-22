3f29cd43fe66b95627063aa34931736e9b0dd7ad
Marcus Tisäter <marcus.tisater@elev.it-gymnasiet.se>
2013-04-22 21:20:44 +0200

    #kodning2
    Funderar på hur jag ska göra med arrayen. Har lagt in devil
    (thumbnail)...

--- printmap.rb
+++ printmap.rb
@@ -1,4 +1,6 @@
+require 'devil'
 class Marcus
+
 
 def ask_name() #funktion för gets.chomp (Fråga vilken mapp)
   puts "Vilken mapp vill du printa ut dina bilder ifron? Ange vart mappen ligger i din dator for att programmet skall lista ut de"
@@ -11,13 +13,28 @@ end
 
 
 def print_file (filename)  #Hela Funktionen för att söka igenom en mapp och printa ut bilderna
-i=0
+	i=0
+	while i <= # Använda arrayen som en slut eftersom vi inte vet hur många bilder som finns. Det måste vara dynamiskt.
+	i = i+1
+	
 	begin
 		Dir.chdir(filename)
-		baba = Dir['*.jpg']  
-		#puts Dir['*gif'] 
+		baba = Dir['*.jpg']    #lagras i en array, som vi sedan kan aropa
+		#puts Dir['*gs if'] 
 		#puts Dir['*png']
-		puts baba[0]
+
+
+
+	#Devil.with_image('*jpg') do |img|
+    #img.thumbnail2(150)
+    #img.gamma_correct(1.6)
+    #img.rotate(90)
+    #img.save("Desert_thumb.jpg")
+
+
+		puts baba[3]                          #anropar Dir.ARRAY
+		puts baba[2]
+
 	rescue                 #om mappen inte finns, frågar den igen som en while loop
 	 puts "Mappen existera inte i din dator. Kontrollera vart den ligger och var vanligen skriv den igen"
 	 file = ask_name()  #anropar funktionen
@@ -25,6 +42,8 @@ i=0
 	end
 end
 end
+end
+
 daniel = Marcus.new  #instans av en class
 henning = Marcus.new 
 
