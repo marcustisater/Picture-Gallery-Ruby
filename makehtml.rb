b544b8fecc4d65405f018fc55b85967c5629db0a
Marcus Tisäter <marcus.tisater@elev.it-gymnasiet.se>
2013-04-22 16:10:46 +0200

    sdfghjk

--- makehtml.rb
+++ makehtml.rb
@@ -5,4 +5,5 @@ fileHtml.puts "<CENTER>this is my crappy webpage</CENTER><br>"
 fileHtml.puts "<CENTER><FONT COLOR='teal'> Jag heter Marcus </FONT></CENTER>"
 fileHtml.puts "</BODY></HTML>"
 fileHtml.close()
+
 system("start marcus.html")
