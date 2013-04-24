447d052c0f648c8ac4578584cf9b2b255d0289ac
Marcus Tisäter <marcus.tisater@elev.it-gymnasiet.se>
2013-04-24 17:46:42 +0200

    kodning#4
    Yes! Thumbnails är fixade. Använde mig av fileutils för att kopia filer
    (flytta filer) etc. Fileutils var smidigt men lite halv svårt att få
    igång. Snyggade också till min kod lite grann. Lade till kommentarer
    också :)

--- printmap.rb
+++ printmap.rb
@@ -1,54 +1,44 @@
 require 'devil'
-class Marcus
+require 'fileutils'
 
+class Searching
+  def ask_name() #funktion för gets.chomp (Fråga vilken mapp)
+    puts "Vilken mapp vill du printa ut dina bilder ifron? Ange vart mappen ligger i din dator for att programmet skall lista ut de och de skapar thumbnails och en html hemsida med dina bilder po"
+    return gets.chomp 
+  end
+  
+  def print_file (filename)  #Funktionen för att söka igenom en mapp och printa ut bilderna
+    begin
+      Dir.chdir(filename)  #Byt till det directory som vi specifiserat
+      sourcefolder=Dir.pwd #Ta ut folder sökvägen
+      bilderna = Dir['*.jpg'] #Hämta alla jpg bilder som finns i foldern och spara dem i en array
+      #puts Dir['*gif']
+      #puts Dir['*png']
+      
+      sourcefolder = sourcefolder + "/*" # lägg till foldersökvägen så den kan användas i Dir.glob och i FilUtils.cp metoderna. 
 
-def ask_name() #funktion för gets.chomp (Fråga vilken mapp)
-  puts "Vilken mapp vill du printa ut dina bilder ifron? Ange vart mappen ligger i din dator for att programmet skall lista ut de"
-  return gets.chomp 
-end
- 
- def balle
- 	puts "Hennig dominerar pa fiol"
- end
-
-
-def print_file (filename)  #Hela Funktionen för att söka igenom en mapp och printa ut bilderna
-	i=0
-	while i <= # Använda arrayen som en slut eftersom vi inte vet hur många bilder som finns. Det måste vara dynamiskt.
-	i = i+1
-	
-	begin
-		Dir.chdir(filename)
-		baba = Dir['*.jpg']    #lagras i en array, som vi sedan kan aropa
-		#puts Dir['*gs if'] 
-		#puts Dir['*png']
-
-		i=0         #Börja iterationen på 0
-      while i < bilderna.length     #itterera så länge i är mindre än längden på array så det fungerar för hur många bilder som helst. Vi vet ju inte hur många bilder som finns i mappen
+      Dir.glob(sourcefolder) {|f| FileUtils.cp File.expand_path(f), "c:/Temp/test" } #Kopierar filerna till en temp folder OBS foldern måste finnas på våran dator     Dir.chdir("C:/Temp/test") # Ändrar directoryt så våra thumbnails skapas i temp/test. 
+      i=0         #Börja iterationen på 0
+      while i < bilderna.length     #itterera så länge i är mindre än längden på array så det fungerar för hur många bilder som helst.
      
       Devil.with_image(bilderna[i]) do |img|  #Devil används för att skapa thumbnails
-            img.thumbnail2(150)
-            img.gamma_correct(1.6)
-            img.save("thumbnail_"+bilderna[i]) #Lägger till thumbnail i namnet för bilderna som blivit thumbnails
+                     img.thumbnail2(150)
+                     img.gamma_correct(1.6)
+                     img.save("thumbnail_"+bilderna[i]) #Lägger till thumbnail i namnet för bilderna som blivit thumbnails
               end 
     i=i+1  #Stega ett steg och kör om while loopen 
     end
-
-	rescue                 #om mappen inte finns, frågar den igen som en while loop
-	 puts "Mappen existera inte i din dator. Kontrollera vart den ligger och var vanligen skriv den igen"
-	 file = ask_name()  #anropar funktionen
-	 print_file(file)  #skriver ut
-	end
+    rescue                 #om mappen inte finns, frågar den igen som en while loop
+     puts "Mappen existera inte i din dator. Kontrollera vart den ligger och var vanligen skriv den igen"
+     file = ask_name()  #anropar funktionen
+     print_file(file)  #skriver ut
+  end
 end
 end
-end
 
-daniel = Marcus.new  #instans av en class
-henning = Marcus.new 
+  
+test = Searching.new()
+folder = test.ask_name()  #anropar funktionen
+test.print_file(folder) #skriver ut
+puts "Whooooo!, Thumbnails finns i C:/Temp/test"
 
-
-file = daniel.ask_name()  #anropar funktionen
-
-#daniel.print_file(bilder[1]) #skriver ut
-#henning.balle #anropar funktionen  #henning innehåller hela classen och kan göra allt men vi säger bara åt honom att göra balle methoden.
-daniel.print_file(file) #skriver ut
