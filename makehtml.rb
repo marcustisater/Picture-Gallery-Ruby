e64a44660c5731b755939d318d267bea2e7ce4a1
Marcus Tisäter <marcus.tisater@elev.it-gymnasiet.se>
2013-04-26 10:03:05 +0200

    Removed old files

--- makehtml.rb
+++
@@ -1,9 +0,0 @@
-
-fileHtml = File.new("marcus.html", "w+")
-fileHtml.puts "<HTML><BODY BGCOLOR='hotpink'>"
-fileHtml.puts "<CENTER>this is my crappy webpage</CENTER><br>"
-fileHtml.puts "<CENTER><FONT COLOR='teal'> Jag heter Marcus </FONT></CENTER>"
-fileHtml.puts "</BODY></HTML>"
-fileHtml.close()
-
-system("start marcus.html")
